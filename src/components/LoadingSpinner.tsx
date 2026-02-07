ba42f1c26c9f249ad180afc2a7c5111bea6d4b57
ismat-samadov <ismetsemedov@gmail.com>
2026-02-07 17:03:09 +0400

    init

--- src/components/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center p-8">
-      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
-    </div>
-  );
-}
